c1b9f1f9e83f0727425278391c13ada564b7005e
Rishiraj Tamuly <107537483+rishirajtamuly@users.noreply.github.com>
2023-11-11 08:27:38 +0530

    Create updation_of_single_char.cpp

---
+++ strings/updation_of_single_char.cpp
@@ -0,0 +1,13 @@
+// Updation of a single character in string
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    string str = "raghav";
+    cout << str << endl;
+    str[1] = 'o';
+    cout << str << endl;
+    return 0;
+}
